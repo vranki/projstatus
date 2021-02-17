2780bbbd6bbbd2017ccc71a9f51813c9e97497eb
Ville Ranki <ville.ranki@iki.fi>
2021-02-17 23:31:29 +0200

    Disable xframe middleware totally

--- projstatus/settings.py
+++ projstatus/settings.py
@@ -47,10 +47,8 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+#    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ROOT_URLCONF = 'projstatus.urls'
 
